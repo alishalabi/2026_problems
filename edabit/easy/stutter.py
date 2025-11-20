dd976163cd37d316695696ff323bfc96f4093a41
Ali Shalabi <ashalabi13@gmail.com>
2025-11-19 16:35:00 -0800

    reorg

--- edabit/easy/stutter.py
+++ edabit/easy/stutter.py
@@ -1,5 +1,6 @@
 """
-Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ... and space after each, and then the word is pronounced with a question mark ?.
+Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ...
+and space after each, and then the word is pronounced with a question mark ?.
 
 Examples
 stutter("incredible") ➞ "in... in... incredible?"
